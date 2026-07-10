f75353f86fb3e637e2b1dc7cc4c31331e8735bca
tsuyoi-w <lenny.wine@icloud.com>
2026-07-11 01:36:41 +0200

    first test generating .h and .cpp with py script need better script

--- tools/makeClass.py
+++ tools/makeClass.py
@@ -8,6 +8,8 @@ import os
 
 ROOT = Path(__file__).parent.parent
 BUILD = ROOT / 'build' / 'src'
+INC_PATH = ROOT / 'includes'
+SRC_PATH = ROOT / 'src'
 
 CODE = Path("./data/main.nso").read_bytes()
 
@@ -16,7 +18,8 @@ func: list[str] = ['\n\n.global {name}\n\n{name}:\n']
 md = Cs(CS_ARCH_ARM64, CS_MODE_ARM) 
 md.skipdata = True
 
-delete = False
+deleteSrc = False
+generateSourceAndHeader = False
 class Function:
     def __init__(self, adress: int, size: int, symbol: str) -> None:
         index = symbol.split('::').__len__() - 1
@@ -36,7 +39,15 @@ with open(ROOT / 'data/rom_extract.csv', newline='', encoding='utf-8') as f:
             FunctionList.append(Function(adress, size, symbol))
 
 AsmFile: list[Path] = []
+create_class = False
+header_str: str = ""
+src_str: str = ""
 for fn in FunctionList:
+    if not create_class:
+        header_str = f"#pragma once\nclass {fn.namespace}" + "{\n"
+        src_str = f'#include "{fn.namespace}.h"\n'
+        namespace = fn.namespace
+        create_class = True
     func_str: str = ""
     data = CODE[fn.adress:fn.adress + fn.size]
     if Path(BUILD / str(fn.namespace+'.s')).exists():
@@ -50,14 +61,34 @@ for fn in FunctionList:
         func.append(f"\t{i.mnemonic} {i.op_str}\n")
     copy = "".join(func)
     func_str += copy
+    
     func_str = func_str.replace('{name}', fn.name)
     func_str = func_str.replace('#', "")
     func = ['\n.global {name}\n{name}:\n']
-    with open(BUILD / str(fn.namespace+'.s'), 'w') as f:
+    if fn.name == "dtor":
+        fn.name = "~" + fn.namespace
+    if fn.name == fn.namespace or fn.name == "~" + fn.namespace:
+        header_str += f"{fn.name}();\n"
+        src_str += f"{fn.namespace}::{fn.name}()" + "{}"
+    else:
+        header_str += f"void {fn.name}();\n"
+        src_str += f"void {fn.namespace}::{fn.name}()" + "{}"
+    with open(BUILD / str(namespace+'.s'), 'w') as f:
         f.write(func_str)
 
-    sp.run(['aarch64-linux-gnu-as', BUILD / str(fn.namespace+'.s'), "-o", BUILD / str(fn.namespace+'.o')])
+header_str += "\n};"
+if Path(INC_PATH / str(namespace+'.h')).exists():
+    print("Header file already exists")
+else:
+    with open(INC_PATH / str(namespace+'.h'), 'w') as f:
+        f.write(header_str)
+if Path(SRC_PATH / str(namespace+'.cpp')).exists():
+    print("Header file already exists")
+else:
+    with open(SRC_PATH / str(namespace+'.cpp'), 'w') as f:
+        f.write(src_str)
+sp.run(['aarch64-linux-gnu-as', BUILD / str(namespace+'.s'), "-o", BUILD / str(namespace+'.o')])
 
-    if delete:
-        for file in AsmFile:
-            os.remove(file)
+if deleteSrc:
+    for file in AsmFile:
+        os.remove(file)
