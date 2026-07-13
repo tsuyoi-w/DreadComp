7e2e7e33584243dacde3cc12ffa896a18cffed12
tsuyoi-w <lenny.wine@icloud.com>
2026-07-13 03:39:48 +0200

    First actor decomp FeelStrongMan

---
+++ includes/ActorUnk_001.h
@@ -0,0 +1,36 @@
+#pragma once
+class ActorUnk_002 {
+public:
+    virtual void vfunc00(int);
+    virtual void vfunc08(int);
+    virtual void vfunc10(int);
+    virtual void vfunc18(int);
+    virtual void vfunc20(int);
+    virtual void vfunc28(int);
+    virtual void vfunc30(int);
+    virtual void vfunc38(int);
+    virtual void * vfunc40(unsigned long, int, int);
+    virtual void vfunc48(long);
+};
+
+class ActorUnk_001 {
+public:
+    /* vtable 0x0 */ 
+    /* 0x08 */ ActorUnk_002* Actor_002;
+    /* 0x10 */ long mUnk_10;
+    /* 0x18 */ long mUnk_18;
+    /* 0x20 */ int mUnk_20;
+    /* 0x24 */ int mUnk_24;
+
+    ActorUnk_001();
+    ActorUnk_001(long, ActorUnk_002, int, int);
+    ~ActorUnk_001();
+
+    virtual void vfunc_08();
+    virtual void vfunc_30(unsigned long);
+    virtual bool vfunc_00();
+    virtual void vfunc_10();
+    virtual void vfunc_18();
+    virtual void vfunc_20();
+    virtual void vfunc_28();
+};  // SIZE 0x28
