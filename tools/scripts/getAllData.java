7460526bc783b2a83c680aa5b577588d7b7e6f5d
Tsuyoi <lenny.wine@icloud.com>
2026-07-25 03:58:31 +0200

    update ghidra scripts

--- tools/scripts/getAllData.java
+++ tools/scripts/getAllData.java
@@ -1,4 +1,4 @@
-//Generates a structure for the vtable
+//Get all symbol and filter it into different csv file
 //@author DisabledMallis
 //@category Symbol
 //@keybinding
@@ -12,48 +12,41 @@ import ghidra.app.script.GhidraScript;
 import ghidra.program.model.address.AddressSetView;
 import ghidra.program.model.listing.Function;
 import ghidra.program.model.listing.FunctionManager;
-import ghidra.program.model.listing.Parameter;
 import ghidra.program.model.listing.Program;
 
 public class getAllData extends GhidraScript {
 
-    final String ex_rom_path = "\\\\wsl.localhost\\DreadComp\\home\\dreadcomp\\projects\\Metroid\\DreadComp\\data\\rom_extract.csv";
-    Boolean ex_rom = true;
-    String ex_rom_str = "";
+    final String fn_path = "\\\\wsl.localhost\\DreadComp\\home\\dreadcomp\\projects\\Metroid\\DreadComp\\data\\function.csv";
+    final String str_path = "\\\\wsl.localhost\\DreadComp\\home\\dreadcomp\\projects\\Metroid\\DreadComp\\data\\string.csv";
+    final String sym_path = "\\\\wsl.localhost\\DreadComp\\home\\dreadcomp\\projects\\Metroid\\DreadComp\\data\\symbol.csv";
+    Boolean fn = true;
+    String fn_str = "";
+    Boolean str = true;
+    String str_str = "";
+    Boolean sym = true;
+    String sym_str = "";
 
-
-    String targetNS = "ActorUnk_001";
-	public void run() throws Exception {
+    public void run() throws Exception {
         Program current = getCurrentProgram();
         FunctionManager fnmngr = current.getFunctionManager();
 
-		if(ex_rom)
-        {
-            for (Function func : fnmngr.getFunctions(true))
-            {
+        if (fn) {
+            for (Function func : fnmngr.getFunctions(true)) {
                 AddressSetView body = func.getBody();
 
                 Long base_addr = Long.parseLong(body.getMinAddress().toString(), 16);
-                Long real_addr = (base_addr - 0x7100000000L) + 0x100;
-                String adrr = Long.toHexString(real_addr);
+                String adrr = Long.toHexString(base_addr);
 
                 String size = Long.toHexString(body.getNumAddresses());
 
-                String namespace = (!func.getParentNamespace().toString().equals("Global")) ? func.getParentNamespace().toString() + "::" : "";
-                String target = (func.getParentNamespace().toString().equals(targetNS)) ? ",true" : ",false";
-                String parameter = "(";
-                int i = 1;
-                for(Parameter pm : func.getParameters())
-                {
-                    String virgule = (func.getParameterCount() == i) ? "" : ",";
-                    parameter += pm.getDataType() + " param_" + i + virgule;
-                    i++;
-                }
+                String namespace = (!func.getParentNamespace().toString().equals("Global"))
+                        ? func.getParentNamespace().toString() + "::"
+                        : "";
 
-                String row = adrr + "," + size + ",\"" + namespace + func.getName() + parameter + ")\"" + target + '\n';
-                ex_rom_str+=row;
+                String row = adrr + "," + size + "," + namespace + func.getName() + '\n';
+                fn_str += row;
             }
-            Files.writeString(Path.of(ex_rom_path), ex_rom_str);       
+            Files.writeString(Path.of(fn_path), fn_str);
         }
 
     }
